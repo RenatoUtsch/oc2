e728587eaa03245a39f2ba66d507594536038f52
Renato Utsch <renatoutsch@gmail.com>
2015-11-04 18:15:12 -0200

    Add Execute_Y signals.

--- tp1/src/Execute_Y.v
+++ tp1/src/Execute_Y.v
@@ -2,6 +2,15 @@ module Execute_Y (
     input               clock,
     input               reset,
     // Issue
+    input       [31:0]  is_y_rega,
+    input       [31:0]  is_y_regb,
+    input       [4:0]   is_y_regdest,
+    input               is_y_writereg,
+    input               is_y_writeov,
+    // Writeback
+    output      [4:0]   y_wb_regdest,
+    output              y_wb_writereg,
+    output      [31:0]  y_wb_wbvalue
 );
 
     Execute_Y0 e_Y0();
