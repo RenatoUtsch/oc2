9ee9052a7f126918b4d1a8413f76d53121d1faa5
epmcj <epmcj@hotmail.com>
2015-11-04 21:05:30 -0200

    Ultimas atualizações.

--- tp1/src/Fetch.v
+++ tp1/src/Fetch.v
@@ -1,6 +1,8 @@
 module Fetch (
     input                   clock,
     input                   reset,
+    //Execute
+    input                   is_if_stall,
     //Decode
     output reg    [31:0]    if_id_nextpc,
     output reg    [31:0]    if_id_instruc,
@@ -28,26 +30,31 @@ module Fetch (
             pc <= 32'h0000_0000;
 				if_id_nextpc <= 32'h0000_0000;
         end else begin
-            if_id_instruc <= data;
-            if (id_if_selpcsource) begin
-                case (id_if_selpctype)
-                    2'b00: pc <= id_if_pcimd2ext;
-                    2'b01: pc <= id_if_rega;
-                    2'b10: pc <= id_if_pcindex;
-                    2'b11: pc <= 32'h0000_0040;
-                    default: pc <= 32'hXXXX_XXXX;
-                endcase
-                case (id_if_selpctype)
-                    2'b00: if_id_nextpc <= id_if_pcimd2ext;
-                    2'b01: if_id_nextpc <= id_if_rega;
-                    2'b10: if_id_nextpc <= id_if_pcindex;
-                    2'b11: if_id_nextpc <= 32'h0000_0040;
-                    default: if_id_nextpc <= 32'hXXXX_XXXX;
-                    //default: pc <= 32'hXXXX_XXXX;
-                endcase
+            if (is_if_stall) begin
+                //if_id_instruc <= 32'h0000_0000;
+                if_id_nextpc <= pc;
             end else begin
-                      pc <= pc + 32'h0000_0001;
-                      if_id_nextpc = pc + 1;
+                if_id_instruc <= data;
+                if (id_if_selpcsource) begin
+                    case (id_if_selpctype)
+                        2'b00: pc <= id_if_pcimd2ext;
+                        2'b01: pc <= id_if_rega;
+                        2'b10: pc <= id_if_pcindex;
+                        2'b11: pc <= 32'h0000_0040;
+                        default: pc <= 32'hXXXX_XXXX;
+                    endcase
+                    case (id_if_selpctype)
+                        2'b00: if_id_nextpc <= id_if_pcimd2ext;
+                        2'b01: if_id_nextpc <= id_if_rega;
+                        2'b10: if_id_nextpc <= id_if_pcindex;
+                        2'b11: if_id_nextpc <= 32'h0000_0040;
+						default: if_id_nextpc <= 32'hXXXX_XXXX;
+                        //default: pc <= 32'hXXXX_XXXX;
+                    endcase
+                end else begin
+						  pc <= pc + 32'h0000_0001;
+						  if_id_nextpc = pc + 1;
+                end
             end
         end
     end
