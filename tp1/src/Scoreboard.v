126bf80ef112cfc5435391e8ad09087230c2c22e
epmcj <epmcj@hotmail.com>
2015-11-04 15:45:30 -0200

    Versão atualizada do Scoreboard.

--- tp1/src/Scoreboard.v
+++ tp1/src/Scoreboard.v
@@ -8,9 +8,9 @@ module Scoreboard(
 );
 
 	/* O SCOREBOARD SERA DIVIDO EM TRES TABELAS. A PRIMEIRA PARTE FICA
-	** RESPONSAVEL POR INFORMAR SE UM REGISTRADOR ESTÁ PENDENTE, A SEGUNDA
-	** EM QUAL UNIDADE FUNCIONAL ESTÁ E A TERCEITA RESPONSAVEL POR MOSTRAR
-	** EM QUAL ESTÁGIO DE EXECUÇÃO ELE ESTÁ.                           */
+	** RESPONSAVEL POR INFORMAR SE UM REGISTRADOR ESTÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â PENDENTE, A SEGUNDA
+	** EM QUAL UNIDADE FUNCIONAL ESTÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â E A TERCEITA RESPONSAVEL POR MOSTRAR
+	** EM QUAL ESTÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚ÂGIO DE EXECUÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€šÃ‚Â¡ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Â ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢O ELE ESTÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â.                           */
 
 	reg [31:0]	     pnd_table;
 	reg [1:0] fun_table [31:0];
@@ -20,16 +20,14 @@ module Scoreboard(
 
 	assign pnd_sgn = pnd_table;
 	
-	
-	always @(negedge clock or negedge reset or negedge wre) begin
+	always @(reset or reg_addr or clock) begin
 		/* Marcando o registrador como pendente e identificando em qual unid.
-		** funcional ele estará.														 */
+		** funcional ele estarÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¡.														 */
 		if(!wre) begin
 			fun_table[reg_addr] <=  func_uni;
 			pnd_table[reg_addr] <= 		 1'b1; 
 			pth_table[reg_addr] <= 6'b100000;
-		end
-			
+		end			
 		// Em caso de reset, zerar todas as tabelas.
 		if(!reset) begin
 			for(i = 0; i < 32; i = i + 1) begin 
@@ -37,19 +35,13 @@ module Scoreboard(
 				fun_table[i] <= 	  2'b00;
 				pth_table[i] <= 6'b000000;
 			end
-
-		end else begin
-			/* Reliza o deslocamento para indicar em qual estágio um dado está e
-			** retira o estado de pendente caso já tenha chegado no Writeback.*/
+		end else if(clock) begin
 			for(j = 0; j < 32; j = j + 1) begin
 				pth_table[j] = pth_table[j] >> 1;
-				pnd_table[j] = pnd_table[j] ^ pth_table[j][0];
-				
+				pnd_table[j] = pnd_table[j] ^ pth_table[j][0];	
 			end
-			
 		end
 		
 	end
-
-
+	
 endmodule
