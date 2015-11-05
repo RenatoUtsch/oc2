25aa2fee9541108a29ff1b0beef78f12c9f8f157
Marcosap19 <pantuzamarcos@gmail.com>
2015-11-04 23:03:43 -0200

    Update Execute_S.v
    corrigindo bugs

--- tp1/src/Execute_S.v
+++ tp1/src/Execute_S.v
@@ -6,7 +6,7 @@ module Execute_S (
     input               in_stall,
     input       [4:0]   in_regdest,
     input               in_writereg,
-    input       [31:0]  in_wbvalue
+    input       [31:0]  in_wbvalue,
     // Output
     output reg          out_stall,
     output reg  [4:0]   out_regdest,
@@ -15,7 +15,12 @@ module Execute_S (
 );
 
 always @(posedge clock or negedge reset) begin
-    if(~reset|in_stall) begin
+    if(~reset) begin
+        out_regdest <= 5'b00000;
+        out_writereg <= 1'b0;
+        out_wbvalue <= 32'h0000_0000;
+        out_stall <= in_stall;
+    end else if(in_stall) begin
         out_regdest <= 5'b00000;
         out_writereg <= 1'b0;
         out_wbvalue <= 32'h0000_0000;
